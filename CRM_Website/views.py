ea51ce36455425af0c0663453d678e613231bef3
Shayan Bagheri <rekklescs@gmail.com>
2025-06-26 11:49:48 +0100

    Enhanced UI; added view/delete individual record features

--- CRM_Website/views.py
+++ CRM_Website/views.py
@@ -23,11 +23,13 @@ def home(request):
 
     else:
         return render(request, 'home.html', {'records': records})
+
     
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
+
 
 def register_user(request):
 	if request.method == 'POST':
@@ -46,3 +48,24 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To View That Page!")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully!")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That!")
+		return redirect('home')
