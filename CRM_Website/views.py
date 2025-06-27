8aac6f8dddd93b5c450b496eb53055744a27f966
Shayan Bagheri <rekklescs@gmail.com>
2025-06-27 13:25:33 +0100

    Enhanced UI, added add/update record function, dark mode function

--- CRM_Website/views.py
+++ CRM_Website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from.forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -56,7 +56,7 @@ def customer_record(request, pk):
         customer_record = Record.objects.get(id=pk)
         return render(request, 'record.html', {'customer_record':customer_record})
     else:
-        messages.success(request, "You Must Be Logged In To View That Page!")
+        messages.success(request, "You Must Be Logged In To View This Page!")
         return redirect('home')
 
 
@@ -67,5 +67,33 @@ def delete_record(request, pk):
 		messages.success(request, "Record Deleted Successfully!")
 		return redirect('home')
 	else:
-		messages.success(request, "You Must Be Logged In To Do That!")
+		messages.success(request, "You Must Be Logged In To View This Page!")
+		return redirect('home')
+      
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added!")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View This Page!")
+		return redirect('home')
+	
+
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View This Page!")
 		return redirect('home')
